cd9b8f3dc5f2c7f9d389af24338f7ce0a0e84a9b
Vivian Lin <linvivian61@gmail.com>
2021-05-25 23:18:18 -0400

    add to cart

--- back-end/src/common-middleware/index.js
+++ back-end/src/common-middleware/index.js
@@ -15,6 +15,7 @@ exports.userMiddleware = (req, res, next) => {
   if (req.user.role !== 'user') {
     return res.status(400).json({ message: 'User Access denied' })
   }
+  next()
 }
 exports.adminMiddleware = (req, res, next) => {
   if (req.user.role !== 'admin') {
