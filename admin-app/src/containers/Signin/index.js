8def21e9fe2c9146461cb9c45a6fff5a9875481e
Vivian Lin <linvivian61@gmail.com>
2021-05-27 13:08:18 -0400

    admin signup

--- admin-app/src/containers/Signin/index.js
+++ admin-app/src/containers/Signin/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Layout from '../../components/Layout'
 import { Container, Form, Row, Col, Button } from 'react-bootstrap'
-
+import Input from '../../components/UI/Input'
 import { login } from '../../actions'
 import { useDispatch, useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
@@ -40,22 +40,21 @@ const Signin = (props) => {
         <Row style={{ marginTop: '50px' }}>
           <Col md={{ span: 6, offset: 3 }}>
             <Form onSubmit={userLogin}>
-              <h5>Email</h5>
-              <FormControl
+              <Input
                 label='Email'
                 placeholder='Email'
                 value={email}
                 type='email'
                 onChange={(e) => setEmail(e.target.value)}
-              ></FormControl>
-              <h5>Password</h5>
-              <FormControl
+              ></Input>
+
+              <Input
                 label='Password'
                 placeholder='Password'
                 value={password}
                 type='password'
                 onChange={(e) => setPassword(e.target.value)}
-              ></FormControl>
+              ></Input>
 
               <Button variant='primary' type='submit'>
                 Submit
--- admin-app/src/containers/Signup/index.js
+++ admin-app/src/containers/Signup/index.js
@@ -1,38 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../components/Layout/index.js'
 import { Container, Form, Button, Row, Col } from 'react-bootstrap'
 import Input from '../../components/UI/Input'
 import { useDispatch, useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { signup } from '../../actions'
 const Signup = (props) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
   const auth = useSelector((state) => state.auth)
+  const user = useSelector((state) => state.user)
+  const dispatch = useDispatch()
+
   if (auth.authenticate) {
     return <Redirect to={`/`} />
+  }
+
+  const userSignup = (e) => {
+    e.preventDefault()
+
+    const user = {
+      firstName,
+      lastName,
+      email,
+      password,
+    }
+
+    dispatch(signup(user))
+  }
+
+  if (user.loading) {
+    return <p>Loading...!</p>
   }
   return (
     <>
       <Layout>
         <Container className='text-center text-md-left'>
+          {user.message}
           <Row style={{ marginTop: '50px' }}>
             <Col md={{ span: 6, offset: 3 }}>
-              <Form>
+              <Form onSubmit={userSignup}>
                 <Row>
                   <Col md={6}>
                     <Input
                       label='First Name'
                       placeholder='First Name'
-                      value=''
+                      value={firstName}
                       type='text'
-                      onChange={(e) => {}}
+                      onChange={(e) => setFirstName(e.target.value)}
                     />
                   </Col>
                   <Col md={6}>
                     <Input
                       label='Last Name'
                       placeholder='Last Name'
-                      value=''
+                      value={lastName}
                       type='text'
-                      onChange={(e) => {}}
+                      onChange={(e) => setLastName(e.target.value)}
                     />
                   </Col>
                 </Row>
@@ -40,17 +67,17 @@ const Signup = (props) => {
                 <Input
                   label='Email'
                   placeholder='Email'
-                  value=''
+                  value={email}
                   type='email'
-                  onChange={(e) => {}}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <Input
                   label='Password'
                   placeholder='Password'
-                  value=''
+                  value={password}
                   type='password'
-                  onChange={(e) => {}}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <Button variant='primary' type='submit'>
                   Submit
